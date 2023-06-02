b332636a008c1d58d08250eb01a38dbc53ae88de
Digilaser <134451451+Digilaser@users.noreply.github.com>
2023-06-02 08:01:50 -0300

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "01 Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/600px-Cat03.jpg",
+    src: "https://github.com/Digilaser/Reproductor/raw/main/music-1.mp3"
+  },
+  {
+    name: "02 Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "https://img.freepik.com/foto-gratis/disparo-gran-angular-solo-arbol-que-crece-cielo-nublado-puesta-sol-rodeada-cesped_181624-22807.jpg",
+    src: "https://github.com/Digilaser/Reproductor/raw/main/music-2.mp3"
+  },
+  {
+    name: "03 Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "https://cdn.pixabay.com/photo/2017/10/10/07/48/hills-2836301_640.jpg",
+    src: "https://github.com/Digilaser/Reproductor/raw/main/music-3.mp3"
+  },
+  {
+    name: "04 Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+    src: "https://github.com/Digilaser/Reproductor/raw/main/music-4.mp3"
+  },
+  {
+    name: "05 Jim - Sun Goes Down capturado",
+    artist: "Jim Yosef x Roy",
+    img: "https://cc-prod.scene7.com/is/image/CCProdAuthor/CODERED_B1_landscape_P2d_714x348.jpg.img?$png$&jpegSize=200&wid=960",
+    src: "https://mp3teca.app/-/aac/96/103689_96.m4a"
+  },
+  {
+    name: "06 Lost Sky - Vision NCS Descargas",
+    artist: "NCS Release",
+    img: "https://i.pinimg.com/originals/c4/3c/2d/c43c2dbe83d7b9c45b9cd028ad2be95f.jpg",
+    src: "https://mp3teca.app/-/mp3/128/Be%C3%A9le%20ft%20Mar%20Lucas%20ft%20Kenia%20Os%20%E2%80%93%20T%C3%BA%20S%C3%AD.mp3"
+  },
+];
